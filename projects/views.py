959845c00317478a6641845ff45f2d245476fc14
maria <sakalovami@gmail.com>
2023-08-07 14:11:05 +0100

    feat(html): Added projects details page and 'read more' button

--- projects/views.py
+++ projects/views.py
@@ -7,3 +7,8 @@ def all_projects(request):
     # Query to DB to return all project
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html', {"projects": projects})
+
+
+def project_details(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/details.html', {"project": project})
